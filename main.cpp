54d65e28638c98f2af27bf4fc9f3658725e51288
TimmyOVO <timmyovo@qq.com>
2020-03-29 20:24:22 +0800

    Application crash issue fix.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,17 @@
 #include "mainwindow.h"
+
 #include <QApplication>
 #include <QFile>
 #include <QTextStream>
 
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
+    QApplication a(argc, argv);
     QFile file(":/light.qss");
     file.open(QFile::ReadOnly | QFile::Text);
     QTextStream stream(&file);
-    app.setStyleSheet(stream.readAll());
-    QApplication a(argc, argv);
+    a.setStyleSheet(stream.readAll());
     MainWindow w;
     w.show();
-
     return QApplication::exec();
 }
